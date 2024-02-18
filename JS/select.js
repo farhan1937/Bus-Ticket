7b96852eb94daa57db7c3a7ca0fe6e6f8679503d
Farhan <farhansadik1937@gmail.com>
2024-02-19 02:40:43 +0600

    added may full design

---
+++ JS/select.js
@@ -0,0 +1,14 @@
+
+// function getSitById(){
+//     const allSites = 'A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4';
+//     const sitItems = allSites.split('');
+//     return sitItems;
+// }
+
+
+
+
+// function setColorById(eventId){
+// const event = document.getElementById(eventId)
+// event.classList.add('bg-[#1DD100]')
+// }
---
+++ JS/ticket.js
@@ -0,0 +1,4 @@
+// function getColorById(){
+//     const colors = getSitById()
+//     setColorById(colors)
+// }
