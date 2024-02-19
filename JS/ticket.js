65b5c712aa80263beb234207d8a3999c2c108b21
Farhan <farhansadik1937@gmail.com>
2024-02-19 12:58:17 +0600

    selected

--- JS/ticket.js
+++ JS/ticket.js
@@ -1,4 +1,15 @@
-// function getColorById(){
-//     const colors = getSitById()
-//     setColorById(colors)
-// }
+let sitsLeft = 40;
+
+const sites = document.getElementById('all-btn')
+
+for(const sit of sites){
+    sit.addEventListener('click', function(e){
+        isCheck = true ;
+          
+        sit.classList.add('bg-[#1DD100]');
+        const selectedPart = document.getElementById('selected');
+        selectedPart.classList.add('text-[#1DD100]');
+        sitsLeft = sitsLeft -1 ;
+        
+    })
+}
